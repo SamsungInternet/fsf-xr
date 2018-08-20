a200559e9c11fa4fc44fe58bc5663c623aa53e05
diekus <diekus@gmail.com>
2018-08-20 17:50:16 +0100

    adds floor

---
+++ panda/js/pandaxr.js
@@ -0,0 +1,22 @@
+let rotDelta = 10;
+let ui_layout = null;
+
+document.addEventListener('DOMContentLoaded', function() {
+    ui_layout = document.querySelector('[a-layout]');
+    rotDelta = 360 / ui_layout.children.length;
+
+    document.querySelector('#btnSeguent').setAttribute('onClick', 'rotar(true)');
+    document.querySelector('#btnAnterior').setAttribute('onClick', 'rotar(false)');
+});
+
+let rotar = function(dir){
+    let currentAngle = toDegrees(ui_layout.object3D.rotation.y);
+    let nAngle = 0;
+    if(dir)
+        nAngle = currentAngle + rotDelta;
+    else
+        nAngle = currentAngle - rotDelta;
+    let = tween_rot = new AFRAME.TWEEN.Tween(ui_layout.object3D.rotation).to({y:toRadians(nAngle)}, 500);
+    tween_rot.easing(TWEEN.Easing.Cubic.Out);
+    tween_rot.start();
+}
